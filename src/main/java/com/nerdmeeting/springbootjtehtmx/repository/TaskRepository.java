5fba0075c9ab31a6a69df69887f93cfee755d9db
Nerd Nathan <nathan@nerdmeeting.com>
2025-01-08 16:39:09 -0700

    Add @Repository to TaskRepository class to add as a bean.

--- src/main/java/com/nerdmeeting/springbootjtehtmx/repository/TaskRepository.java
+++ src/main/java/com/nerdmeeting/springbootjtehtmx/repository/TaskRepository.java
@@ -2,10 +2,12 @@ package com.nerdmeeting.springbootjtehtmx.repository;
 
 import com.nerdmeeting.springbootjtehtmx.entity.Task;
 import jakarta.annotation.PostConstruct;
+import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Repository
 public class TaskRepository {
     
     private final List<Task> tasks = new ArrayList<>();
