a65590e35b3b816273cfe7ce3b4793b81820f345
Nerd Nathan <nathan@nerdmeeting.com>
2025-01-08 18:58:11 -0700

    Add new POST request endpoint to create a new task.

--- src/main/java/com/nerdmeeting/springbootjtehtmx/controller/TaskController.java
+++ src/main/java/com/nerdmeeting/springbootjtehtmx/controller/TaskController.java
@@ -1,11 +1,14 @@
 package com.nerdmeeting.springbootjtehtmx.controller;
 
+import com.nerdmeeting.springbootjtehtmx.entity.Task;
 import com.nerdmeeting.springbootjtehtmx.repository.TaskRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class TaskController {
@@ -21,5 +24,13 @@ public class TaskController {
     public String index(Model model) {
         model.addAttribute("tasks", taskRepository.findAll());
         return "index";
+    }
+    
+    @PostMapping("/add-task")
+    public String addTask(@RequestParam String description, Model model) {
+        Task task = new Task(description);
+        taskRepository.create(task);
+        model.addAttribute("task", task);
+        return "task-row";
     }
 }
