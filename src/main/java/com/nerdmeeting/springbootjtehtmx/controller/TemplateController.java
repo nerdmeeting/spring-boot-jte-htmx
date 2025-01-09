2915ee8a41808ae8c7fc061ad6ff4ea2e2c4cd46
Nerd Nathan <nathan@nerdmeeting.com>
2025-01-09 00:38:05 -0700

    Add projects.jte and @GetMapping for /projects with some sample projects so the Projects top-menu link works.

--- src/main/java/com/nerdmeeting/springbootjtehtmx/controller/TemplateController.java
+++ src/main/java/com/nerdmeeting/springbootjtehtmx/controller/TemplateController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/")
@@ -23,5 +24,16 @@ public class TemplateController {
         List<String> teamMembers = List.of("Alice", "Jose", "Charlie", "Michell", "Lauris");
         model.addAttribute("teamMembers", teamMembers);
         return "pages/team";
+    }
+    
+    @GetMapping("/projects")
+    public String projects(Model model) {
+        List<String> projects = List.of("Project 1", "Project 2", "Project 3", "Project 4");
+        Map<String, Object> attributes = Map.of(
+                "username", "John Doe",
+                "projects", projects
+        );
+        model.addAllAttributes(attributes);
+        return "pages/projects";
     }
 }
