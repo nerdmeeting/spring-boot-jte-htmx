3d68e031238f008caa4b21497d1426ab009d4443
Nerd Nathan <nathan@nerdmeeting.com>
2025-01-08 18:12:28 -0700

    Fix Lombok in the Gradle dependencies so it works and the app can start.

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:3.4.1")
     implementation("gg.jte:jte:3.1.12")
     implementation("gg.jte:jte-spring-boot-starter-3:3.1.12")
-    implementation("org.projectlombok:lombok:1.18.36")
+    compileOnly("org.projectlombok:lombok:1.18.36")
+    annotationProcessor("org.projectlombok:lombok:1.18.36")
     testImplementation("org.springframework.boot:spring-boot-starter-test:3.4.1")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher:1.11.4")
 }
